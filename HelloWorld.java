dd9489dbcd0b98d6da4c026230335ec6e286441f
imanfirmansyah <bwekz.pirmansyah@gmail.com>
2017-03-01 00:32:52 +0700

    versi 1.0

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+class mahasiswa{
+    (string []argv){
+        system.out.printl("Hello World");
+    }
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+    public static void main (string[] argv) {
+        system.out.println("Nama Lengkap : Iman Pirmansyah");
+        system.out.println("NIM          : 10112772 ");
+        system.out.println("Alamat       : jl.Sekeloa Bandung");
+    }
+}
